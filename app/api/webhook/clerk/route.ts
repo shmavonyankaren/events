12ef9ebad0555ed5f1d60f7667d8128ec6f67ed9
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-03-05 09:16:42 +0400

    fixing clerk auth

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -84,7 +84,7 @@ export async function POST(req: Request) {
     }
     alert("finally done");
 
-    return NextResponse.json({ message: "OK", user: newUser });
+    return NextResponse.json({ message: "Received" });
   }
 
   if (eventType === "user.updated") {
@@ -100,7 +100,7 @@ export async function POST(req: Request) {
 
     const updatedUser = await updateUser(id, user);
 
-    return NextResponse.json({ message: "OK", user: updatedUser });
+    return NextResponse.json({ message: "Received" });
   }
 
   if (eventType === "user.deleted") {
@@ -109,7 +109,7 @@ export async function POST(req: Request) {
 
     const deletedUser = await deleteUser(id!);
 
-    return NextResponse.json({ message: "OK", user: deletedUser });
+    return NextResponse.json({ message: "Received" });
   }
 
   return new Response("", { status: 200 });
