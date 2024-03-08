6de62201229c05932eb10eb17a0b628e784d2037
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-03-08 07:14:48 +0400

    change the css and add new logo, also add loading animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "Events",
   description: "Events is a platform for event management.",
   icons: {
-    icon: "/assets/images/logo.svg",
+    icon: "/assets/images/logo.png",
   },
 };
 
---
+++ app/loading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex w-[100%] h-[80vh] justify-center items-center">
+      <div className="container-loader">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </div>
+  );
+}
