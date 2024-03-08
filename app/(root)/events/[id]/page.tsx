8c02b0587ca435b81d7d8364b1befeb75fa30b41
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-03-08 08:58:27 +0400

    fix responsive design

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -24,16 +24,15 @@ const EventDetails = async ({
   return (
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-        <div className="grid grid-cols-1  md:grid-cols-2 2xl:max-w-7xl mt-0 mr-2 ml-2">
+        <div className="grid grid-cols-1  md:grid-cols-2 2xl:max-w-7xl">
           <Image
             src={event.imageUrl}
             alt="hero image"
             width={1000}
             height={1000}
-            className="h-full min-h-[300px] object-cover object-center rounded-lg  m-auto max-h-[500px] "
+            className="h-full min-h-[300px] object-cover object-center rounded-lg max-h-[500px] "
           />
-
-          <div className="flex w-full flex-col gap-8 p-5 md:p-10 pl-5">
+          <div className="flex w-full flex-col gap-8 p-5 md:p-10">
             <div className="flex flex-col gap-6">
               <h2 className="h2-bold">{event.title}</h2>
 
